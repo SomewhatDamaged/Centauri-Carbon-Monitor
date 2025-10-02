6b06d34313d50eb0f98a1e4a29072839a479dd36
SomewhatDamaged <not.2.me.damaged@gmail.com>
2025-10-02 23:54:41 +1000

    Working!

--- src/carbon.py
+++ src/carbon.py
@@ -1,5 +1,9 @@
 import asyncio
 import json
+import logging
+import sys
+import time
+import uuid
 from dataclasses import asdict, dataclass
 from typing import Union
 
@@ -12,6 +16,7 @@ STATUS = {
     13: "Printing",
     9: "Complete",
     0: "Idle",
+    6: "Paused",
 }
 
 
@@ -22,7 +27,6 @@ class Stats:
     enclosure_temp: float = 0.0
     target_nozzle_temp: float = 0.0
     target_bed_temp: float = 0.0
-    target_enclosure_temp: float = 0.0
     z_offset: float = 0.0
     model_fan_speed: int = 0
     aux_fan_speed: int = 0
@@ -33,26 +37,32 @@ class Stats:
     total_layers: int = 0
     print_speed: str = "0%"
     elapsed_time: str = "0"
+    elapsed_time_raw: int = 0
     remaining_time: str = "0"
+    remaining_time_raw: int = 0
+    total_time: str = "0"
+    total_time_raw: int = 0
 
 
 class CarbonData:
     target: str = None
-    url = "ws://{self.target}:3030/websocket"
+    url = "ws://{target}:3030/websocket"
     data = Stats()
     error: str = None
-    _video: str = "http://{self.target}:3031/video"
+    _video: str = "http://{target}:3031/video"
     _connected = asyncio.Lock()
+    _log: logging.Logger = None
+    _connection: aiohttp.ClientWebSocketResponse
 
-    @getattr
+    @property
     def to_dict(self) -> dict:
         return asdict(self.data)
 
-    @getattr
+    @property
     def video(self) -> Union[str, None]:
-        return self._video.format() if self.connected else None
+        return self._video.format(target=self.target) if self.connected else None
 
-    @getattr
+    @property
     def connected(self) -> bool:
         return self._connected.locked()
 
@@ -62,31 +72,77 @@ class CarbonData:
 
     async def connect(self, timeout: int = 0) -> None:
         session = aiohttp.ClientSession()
+        tickler = None
         try:
             while not self.target:
-                await asyncio.sleep(5)
-            connection = session.ws_connect(url=self.url.format())
-            await self._connected.acquire()
-            async for message in connection:
-                self.ws_process_message(message)
+                await asyncio.sleep(1)
+            self.log(f"Connecting to: {self.url.format(target=self.target)}")
+            async with session.ws_connect(
+                url=self.url.format(target=self.target)
+            ) as self._connection:
+                tickler = asyncio.create_task(self.request_info())
+                self.log("Connected!")
+                await self._connected.acquire()
+                self.log("Locked!")
+                async for message in self._connection:
+                    self.ws_process_message(message)
         except asyncio.CancelledError:
+            self.log("Exiting!")
             """closing"""
             return
-        except aiohttp.client_exceptions:
+        except aiohttp.ClientError:
+            exc_type, _, _ = sys.exc_info()
+            self.log("HTTP error!")
+            if self._log:
+                self._log.exception(exc_type)
+            self.target = None
             await asyncio.sleep(timeout)
             return asyncio.create_task(self.connect(timeout=max(timeout * timeout, 2)))
-        except Exception as e:
-            self.error = e
+        except Exception:
+            exc_type, _, _ = sys.exc_info()
+            self.log("Error!")
+            if self._log:
+                self._log.exception(exc_type)
+        else:
+            return asyncio.create_task(self.connect(timeout=max(timeout * timeout, 2)))
         finally:
+            self.log("Closing session")
             await session.close()
-            self._connected.release()
+            self.log("Checking lock...")
+            if self._connected.locked():
+                self.log("Freeing lock!")
+                self._connected.release()
+            if not tickler.done():
+                tickler.cancel()
+
+    async def request_info(self) -> None:
+        try:
+            while True:
+                await asyncio.sleep(2)
+                await self._connection.send_json(
+                    {
+                        "Id": "",
+                        "Data": {
+                            "Cmd": 0,
+                            "Data": {},
+                            "RequestID": uuid.uuid4().hex,
+                            "MainboardID": "",
+                            "TimeStamp": int(time.time() * 1000),
+                            "From": 1,
+                        },
+                    }
+                )
+        except asyncio.CancelledError:
+            return "Exiting"
 
     def ws_process_message(self, message: aiohttp.WSMessage) -> None:
+        self.log("Processing a message...")
         if message.type != aiohttp.WSMsgType.TEXT:
             return
         data = json.loads(message.data)
         try:
             self.process_data(data)
+            self.log(f"Processed message: {self.to_dict}")
         except AssertionError:
             return
 
@@ -107,11 +163,16 @@ class CarbonData:
         print_speed = int(print_info.get("PrintSpeedPct", 0))
         current_layer = int(print_info.get("CurrentLayer", 0))
         total_layers = int(print_info.get("TotalLayer", 0))
+        elapsed_time = int(print_info.get("CurrentTicks", 0))
 
         self.data.print_status = STATUS.get(print_status, f"Unknown: {print_status}")
         self.data.progress = round(print_info.get("Progress", 0.0), 1)
-        self.data.elapsed_time = time_format(int(print_info.get("CurrentTicks", "0")))
-        self.data.remaining_time = time_format(max(0, time - self.data.elapsed_time))
+        self.data.elapsed_time = time_format(elapsed_time)
+        self.data.elapsed_time_raw = elapsed_time
+        self.data.remaining_time = time_format(max(0, time - elapsed_time))
+        self.data.remaining_time_raw = max(0, time - elapsed_time)
+        self.data.total_time = time_format(time)
+        self.data.total_time_raw = time
         self.data.aux_fan_speed = fan_speeds.get("AuxiliaryFan", 0)
         self.data.box_fan_speed = fan_speeds.get("BoxFan", 0)
         self.data.model_fan_speed = fan_speeds.get("ModelFan", 0)
@@ -139,18 +200,22 @@ class CarbonData:
             total_layers if total_layers else self.data.total_layers
         )
 
+    def log(self, message: str):
+        if self._log:
+            self._log.debug(message)
+
 
 def time_format(seconds: int) -> str:
     output_string: list = []
     if seconds > 86400:
-        output_string.append(f"{seconds%86400}d")
-        seconds = seconds // 86400
+        output_string.append(f"{seconds//86400}d")
+        seconds = seconds % 86400
     if seconds > 3600:
-        output_string.append(f"{seconds%3600}h")
-        seconds = seconds // 3600
+        output_string.append(f"{seconds//3600}h")
+        seconds = seconds % 3600
     if seconds > 60:
-        output_string.append(f"{seconds%60}m")
-        seconds = seconds // 60
+        output_string.append(f"{seconds//60}m")
+        seconds = seconds % 60
     if seconds:
         output_string.append(f"{seconds}s")
     return " ".join(output_string)
--- src/main.py
+++ src/main.py
@@ -1,6 +1,27 @@
+import asyncio
+import logging
+import logging.handlers
 from typing import Union
 
 import flet as ft
+
+from carbon import CarbonData
+
+log = logging.getLogger("Carbon")
+log.setLevel(logging.DEBUG)
+fhandler = logging.handlers.RotatingFileHandler(  # type: ignore
+    filename="carbon.log",
+    encoding="utf-8",
+    mode="a",
+    maxBytes=10**7,
+    backupCount=5,
+)
+fmt = logging.Formatter(
+    "%(asctime)s %(levelname)s %(module)s %(funcName)s %(lineno)d: %(message)s",
+    datefmt="[%d/%m/%Y %H:%M]",
+)
+fhandler.setFormatter(fmt)
+log.addHandler(fhandler)
 
 
 class Monitor:
@@ -9,30 +30,127 @@ class Monitor:
             keyboard_type=ft.KeyboardType.URL, on_submit=self.set_ip
         )
         self.show_ip = ft.Text(value="None", visible=False)
+        self.printer = CarbonData()
+        self.printer._log = log
 
-    ip_address: Union[str, None] = None
+    def process_data(self) -> None:
+        data = self.printer.data
+        self.status.value = f"Status: {data.print_status}"
+        self.layer_progress_text.value = (
+            f"Layer: {data.current_layer}/{data.total_layers}"
+        )
+        self.layer_progress.value = (
+            float(data.current_layer / data.total_layers) if data.total_layers else 0.0
+        )
+        self.temperatures.value = f"Temperature\nNozzle: {data.nozzle_temp}C/{data.target_nozzle_temp}C\nBed: {data.bed_temp}C/{data.target_bed_temp}C\nBox: {data.enclosure_temp}C"
+        self.fans.value = f"Fan Speeds\nModel: {data.model_fan_speed}%\nAux: {data.aux_fan_speed}%\nBox: {data.box_fan_speed}%"
+        self.z_offset.value = f"Z-Offset: {data.z_offset}"
+        self.progress_text.value = f"Progress: {data.progress}%"
+        self.progress.value = data.progress / 100.0
+        self.time_text.value = f"Time remaining: {data.remaining_time}"
+        self.time.value = (
+            float(data.elapsed_time_raw / data.total_time_raw)
+            if data.total_time_raw
+            else 0
+        )
+        self._data_layout.update()
+
+    def data_layout(self) -> None:
+        data = self.printer.data
+        self.status = ft.Text(value=f"Status: {data.print_status}")
+        self.layer_progress_text = ft.Text(
+            value=f"Layer: {data.current_layer}/{data.total_layers}"
+        )
+        self.layer_progress = ft.ProgressBar(
+            color=ft.Colors.GREEN,
+            value=(
+                float(data.current_layer / data.total_layers)
+                if data.total_layers
+                else 0.0
+            ),
+        )
+        self.temperatures = ft.Text(
+            value=f"Temperature\nNozzle: {data.nozzle_temp}C/{data.target_nozzle_temp}C\nBed: {data.bed_temp}C/{data.target_bed_temp}C\nBox: {data.enclosure_temp}C"
+        )
+        self.fans = ft.Text(
+            value=f"Fan Speeds\nModel: {data.model_fan_speed}\nAux: {data.aux_fan_speed}\nBox: {data.box_fan_speed}"
+        )
+        self.z_offset = ft.Text(value=f"Z-Offset: {data.z_offset}")
+        self.progress_text = ft.Text(value=f"Progress: {data.progress}%")
+        self.progress = ft.ProgressBar(
+            color=ft.Colors.GREEN,
+            value=data.progress / 100.0,
+        )
+        self.time_text = ft.Text(value=f"Time remaining: {data.remaining_time}")
+        self.time = ft.ProgressBar(
+            color=ft.Colors.GREEN,
+            value=(
+                float(data.elapsed_time_raw / data.total_time_raw)
+                if data.total_time_raw
+                else 0.0
+            ),
+        )
+        self._data_layout = ft.Column(
+            controls=[
+                self.status,
+                self.layer_progress_text,
+                self.layer_progress,
+                self.progress_text,
+                self.progress,
+                self.time_text,
+                self.time,
+                self.temperatures,
+                self.fans,
+            ],
+            visible=False,
+        )
+
+    async def update_data(self) -> None:
+        while True:
+            try:
+                while not self.printer.connected:
+                    if self._data_layout.visible:
+                        self._data_layout.visible = False
+                        self._data_layout.update()
+                    await asyncio.sleep(0.5)
+                if not self._data_layout.visible:
+                    self._data_layout.visible = True
+                    self._data_layout.update()
+                self.process_data()
+                await asyncio.sleep(1)
+            except asyncio.CancelledError:
+                return "Exiting..."
+
+    target: Union[str, None] = None
 
     async def set_ip(self, event: ft.ControlEvent) -> None:
-        self.ip_address = event.control.value
-        self.show_ip.value = self.ip_address
-        self.show_ip.visible = True
-        self.update()
+        self.select_ip.tooltip = event.control.value
+        self.select_ip.value = event.control.value
+        self.printer.target = event.control.value
+        self.select_ip.update()
 
     async def main(self, page: ft.Page):
+        log.debug("Starting!")
         page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
         page.title = "Centauri Carbon Monitor"
-        page.window.width = 720
-        page.window.height = 1080
+        page.window.width = 400
+        page.window.height = 500
         page.theme_mode = ft.ThemeMode.DARK
+        log.debug("Basic layout done")
+        self.data_layout()
 
         page.add(self.select_ip)
-        page.add(self.show_ip)
+        page.add(ft.Divider())
+        self.data_layout()
+        page.add(self._data_layout)
+        page.update()
         self.page = page
-        self.update()
-
-    def update(self) -> None:
-        self.page.update()
+        asyncio.create_task(self.update_data())
 
 
-app = Monitor()
-ft.app(target=app.main)
+async def runner():
+    app = Monitor()
+    await ft.app_async(target=app.main)
+
+
+asyncio.run(runner())

adc7f97adfe7775a26bac936e41185124f63b8dc
SomewhatDamaged <not.2.me.damaged@gmail.com>
2025-10-04 00:00:04 +1000

    Adjusting what elements are visible during what states.

--- src/main.py
+++ src/main.py
@@ -23,13 +23,13 @@ fmt = logging.Formatter(
 fhandler.setFormatter(fmt)
 log.addHandler(fhandler)
 
-COLORS = [
-    ft.Colors.RED,
-    ft.Colors.ORANGE,
-    ft.Colors.YELLOW,
-    ft.Colors.LIME,
-    ft.Colors.GREEN,
-]
+COLORS = {
+    0: ft.Colors.RED,
+    1: ft.Colors.ORANGE,
+    2: ft.Colors.YELLOW,
+    3: ft.Colors.LIME,
+    4: ft.Colors.GREEN,
+}
 STATUS = {
     "Printing": ft.Colors.RED,
     "Paused": ft.Colors.YELLOW,
@@ -37,7 +37,7 @@ STATUS = {
     "Resuming": ft.Colors.YELLOW,
     "Idle": ft.Colors.GREY,
     "Preparing": ft.Colors.ORANGE,
-    "Completed": ft.Colors.GREEN,
+    "Complete": ft.Colors.GREEN,
 }
 
 
@@ -50,10 +50,10 @@ class Monitor:
 
     def process_data(self) -> None:
         data = self.printer.data
-        if self.status != "Unknown":
-            self._data_layout.visible = True
-        else:
+        if data.print_status.startswith("Unknown"):
             self._data_layout.visible = False
+        else:
+            self._data_layout.visible = True
         self.status.value = f"Status: {data.print_status}"
         self.status.color = STATUS.get(data.print_status, ft.Colors.WHITE)
         if data.print_status in [
@@ -67,34 +67,50 @@ class Monitor:
             self.layer_progress.visible = True
             self.progress_text.visible = True
             self.progress.visible = True
+            self.time_text.visible = True
+            self.layer_progress_text.value = (
+                f"Layer: {data.current_layer} / {data.total_layers}"
+            )
+            self.time_text.value = f"Time: {data.remaining_time} / {data.total_time}"
         elif data.print_status == "Complete":
             self.layer_progress_text.visible = True
+            self.time_text.visible = True
             self.layer_progress.visible = False
             self.progress_text.visible = False
             self.progress.visible = False
+            self.fans.visible = True
             self.layer_progress_text.value = f"Completed {data.total_layers} layers!"
+            self.time_text.value = f"Time: {data.elapsed_time}"
+        elif data.print_status == "Idle":
+            self.layer_progress_text.visible = False
+            self.time_text.visible = False
+            self.layer_progress.visible = False
+            self.progress_text.visible = False
+            self.progress.visible = False
+            self.fans.visible = False
+            self.time_text.visible = False
         else:
             self.layer_progress_text.visible = False
             self.layer_progress.visible = False
             self.progress_text.visible = False
             self.progress.visible = False
-        self.layer_progress_text.value = (
-            f"Layer: {data.current_layer} / {data.total_layers}"
-        )
+            self.fans.visible = False
         self.layer_progress.value = (
             float(data.current_layer / data.total_layers) if data.total_layers else 0.0
         )
         if data.total_layers:
-            self.layer_progress.color = COLORS[
-                int(round((data.current_layer / data.total_layers * 20) % 5, 0))
-            ]
+            self.layer_progress.color = COLORS.get(
+                int((data.current_layer / data.total_layers * 4.9) % 5),
+                ft.Colors.GREEN,
+            )
         self.temperatures.value = f"Temperature\nNozzle: {data.nozzle_temp}C / {data.target_nozzle_temp}C\nBed: {data.bed_temp}C / {data.target_bed_temp}C\nBox: {data.enclosure_temp}C"
         self.fans.value = f"Fan Speeds\nModel: {data.model_fan_speed}%\nAux: {data.aux_fan_speed}%\nBox: {data.box_fan_speed}%"
         self.z_offset.value = f"Z-Offset: {data.z_offset}"
         self.progress_text.value = f"Progress: {data.progress}%"
         self.progress.value = data.progress / 100.0
-        self.progress.color = COLORS[int(round((data.progress / 20) % 5, 0))]
-        self.time_text.value = f"Time: {data.remaining_time} / {data.total_time}"
+        self.progress.color = COLORS.get(
+            int((data.progress / 20.1) % 5), ft.Colors.GREEN
+        )
         self._data_layout.update()
 
     def data_layout(self) -> None:
@@ -138,6 +154,7 @@ class Monitor:
                 self.fans,
             ],
             visible=False,
+            horizontal_alignment=ft.CrossAxisAlignment.START,
         )
 
     async def update_data(self) -> None:
@@ -170,6 +187,7 @@ class Monitor:
         page.window.width = 400
         page.window.height = 500
         page.theme_mode = ft.ThemeMode.DARK
+        page.horizontal_alignment = ft.CrossAxisAlignment.START
         self.data_layout()
 
         page.add(self.select_ip)
